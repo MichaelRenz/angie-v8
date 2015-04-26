c6e605c301f4e8bf9ea2a3b0e1dfe4ab4950c855
El Shotodore <git@exit13.de>
2015-04-26 10:04:33 +0200

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+/**
+ * Created by elshotodore on 26.04.15.
+ */
+public class Application {
+
+    public static void main ( String[] args) {
+        System.out.println("Application running...");
+    }
+}
