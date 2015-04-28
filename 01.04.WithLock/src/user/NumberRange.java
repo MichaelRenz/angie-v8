af6b084186053cae4538ba972af42aa02d8eecbc
El Shotodore <elshotodore@users.noreply.github.com>
2015-04-28 13:32:18 +0200

    Update NumberRange.java
    solved

--- 01.04.WithLock/src/user/NumberRange.java
+++ 01.04.WithLock/src/user/NumberRange.java
@@ -3,7 +3,7 @@
   and owned by Angelika Langer & Klaus Kreft.
   contact: http://www.AngelikaLanger.com/ or mailto: info@AngelikaLanger.com
 
-  © Copyright 2013-2014 by Angelika Langer & Klaus Kreft. All rights reserved.
+  Â© Copyright 2013-2014 by Angelika Langer & Klaus Kreft. All rights reserved.
 
   Permission to use, copy, and modify this software for any non-profit
   purpose is hereby granted to attendants of the above mentioned seminar
@@ -11,91 +11,131 @@
   copies.  Angelika Langer and Klaus Kreft make no representations about
   the suitability of this software for any purpose.  It is provided
   "as is" without express or implied warranty.
-*/
+ */
 package user;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
 
 public final class NumberRange {
-    public static class BrokenRangeException extends Exception {
-        public BrokenRangeException(NumberRange range) {
-            super(range.toString());
-        }
-    }
-    private final Lock lock = new ReentrantLock();
-    private volatile int lower = 0;
-    private volatile int upper = 0;
+	public static class BrokenRangeException extends Exception {
+		public BrokenRangeException(NumberRange range) {
+			super(range.toString());
+		}
+	}
 
-    public NumberRange() {
-        super();
-    }
-    public NumberRange(int l, int u) throws BrokenRangeException {
-        lower=l;
-        upper=u;
-        isValid();
-    }
+	private final Lock lock = new ReentrantLock();
+	private volatile int lower = 0;
+	private volatile int upper = 0;
 
-    public void setLower(int i) {
-        lock.lock();
-        try {
-            if (i > upper)
-                throw new IllegalArgumentException(
-                        "can't set lower to " + i + " > upper");
-            lower=i;
-        } finally {
-            lock.unlock();
-        }
-    }
-    public void setUpper(int i) {
-        lock.lock();
-        try {
-            if (i < lower)
-                throw new IllegalArgumentException(
-                        "can't set upper to " + i + " < lower");
-            upper=i;
-        } finally {
-            lock.unlock();
-        }
-    }
-    public int getLower() {
-        return lower;
-    }
-    public int getUpper() {
-        return upper;
-    }
-    public int[] getRange() {
-        lock.lock();
-        try {
-            return new int[] {lower,upper};
-        } finally {
-            lock.unlock();
-        }
-    }
+	public NumberRange() {
+		super();
+	}
 
-    public String toString() {
-        lock.lock();
-        try {
-            return "["+lower+","+upper+"]";
-        } finally {
-            lock.unlock();
-        }
-    }
-    public boolean isInRange(int i) {
-        lock.lock();
-        try {
-            return (i >= lower && i <= upper);
-        } finally {
-            lock.unlock();
-        }
-    }
-    public boolean isValid() throws BrokenRangeException {
-        lock.lock();
-        try {
-            if (lower <= upper) return true;
-            else throw new BrokenRangeException(this);
-        } finally {
-            lock.unlock();
-        }
-    }
+	public NumberRange(int l, int u) throws BrokenRangeException {
+		lower = l;
+		upper = u;
+		isValid();
+	}
+
+	public void setLower(int i) {
+		handleLockUnlockWithExceptionVoid(() -> {
+			if (i > upper)
+				throw new IllegalArgumentException("can't set lower to " + i
+						+ " > upper");
+			lower = i;
+		});
+	}
+
+	public void setUpper(int i) {
+		handleLockUnlockWithExceptionVoid(() -> {
+			if (i < lower)
+				throw new IllegalArgumentException("can't set upper to " + i
+						+ " < lower");
+			upper = i;
+		});
+
+	}
+
+	public int getLower() {
+		return lower;
+	}
+
+	public int getUpper() {
+		return upper;
+	}
+
+	public int[] getRange() {
+		return handleLockUnlock(() -> {
+			return new int[] { lower, upper };
+		});
+
+		// lock.lock();
+		// try {
+		// return new int[] {lower,upper};
+		// } finally {
+		// lock.unlock();
+		// }
+	}
+
+	public String toString() {
+		return handleLockUnlock(() -> {
+			return "[" + lower + "," + upper + "]";
+		});
+
+	}
+
+	public boolean isInRange(int i) {
+		return handleLockUnlock(() -> {
+			return (i >= lower && i <= upper);
+		});
+	}
+
+	public boolean isValid() throws BrokenRangeException {
+		return handleLockUnlockWithException(() -> {
+			if (lower <= upper)
+				return true;
+			else
+				throw new BrokenRangeException(this);
+		});
+	}
+
+	<T> T handleLockUnlockWithException(
+			ReflectiveExecutionWithException<T> action) {
+		lock.lock();
+		try {
+			return action.run();
+		} catch (IllegalArgumentException iae) {
+			iae.printStackTrace();
+		} catch (BrokenRangeException bre) {
+			bre.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+		return null;
+
+	}
+	
+	void handleLockUnlockWithExceptionVoid(
+			ReflectiveExecutionVoid action) {
+		lock.lock();
+		try {
+			action.run();
+		} catch (IllegalArgumentException iae) {
+			iae.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	<T> T handleLockUnlock(ReflectiveExecution<T> action) {
+		lock.lock();
+		try {
+			return action.run();
+		} finally {
+			lock.unlock();
+		}
+
+	}
 }
